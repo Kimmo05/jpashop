64095df78194efeedbb82aa90a5f59ceed17398b
Kimmo05 <longdali05@naver.com>
2021-09-15 18:32:50 +0900

    Initial commit

---
+++ src/main/java/jpaproject/jpashop/domain/Member.java
@@ -0,0 +1,24 @@
+package jpaproject.jpashop.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+    private String loginId;
+    private String password;
+    private String name;
+    private String email;
+
+}

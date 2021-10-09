bb6b2983ad26edc28122228d8f4103d50f4c0a95
kimmo05 <longdali05@naver.com>
2021-10-09 22:14:30 +0900

    test

--- src/main/java/jpaproject/jpashop/domain/Category.java
+++ src/main/java/jpaproject/jpashop/domain/Category.java
@@ -2,7 +2,9 @@ package jpaproject.jpashop.domain;
 
 
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,9 +13,11 @@ import java.util.List;
 
 @Entity
 @Getter @Setter
+@Table(name = "categories")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Category {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="category_id")
     private Long id;
 

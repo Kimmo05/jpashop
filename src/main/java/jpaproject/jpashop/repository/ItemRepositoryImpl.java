9167704113e1afe62c1772d43323853daf50475c
kimmo05 <longdali05@naver.com>
2021-10-23 22:04:19 +0900

    item

--- src/main/java/jpaproject/jpashop/repository/ItemRepositoryImpl.java
+++ src/main/java/jpaproject/jpashop/repository/ItemRepositoryImpl.java
@@ -1,13 +1,49 @@
 package jpaproject.jpashop.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jpaproject.jpashop.domain.QItem;
+import jpaproject.jpashop.dto.ItemDto;
+import jpaproject.jpashop.dto.QItemDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class ItemRepositoryImpl implements ItemRepositoryCustom {
     private final JPAQueryFactory queryFactory;
     public ItemRepositoryImpl(EntityManager em){
         this.queryFactory = new JPAQueryFactory(em);
     }
-    
+
+    @Override
+    public Page<ItemDto> searchAllItem(Pageable pageable) {
+        QueryResults<ItemDto> results = queryFactory
+                .select(new QItemDto(
+                        QItem.item.id,
+                        QItem.item.itemName,
+                        QItem.item.firstCategory,
+                        QItem.item.price,
+                        QItem.item.saleStatus,
+                        QItem.item.imgUrl,
+                        QItem.item.color,
+                        QItem.item.rep,
+                        QItem.item.itemIdx
+                ))
+                .from(QItem.item)
+                .where(QItem.item.rep.eq(true))
+                .orderBy(QItem.item.id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+
+        List<ItemDto> content = results.getResults();
+        long total = results.getTotal();
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+
 }
